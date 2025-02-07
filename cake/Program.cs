be20e07ec2ea035dd0d062bbef3c14171117e80d
PTKu <61538034+PTKu@users.noreply.github.com>
2025-02-07 15:50:45 +0100

    Update NuGet API key and repository name in PushTcOpenGroupPackages and ReleaseTask

--- cake/Program.cs
+++ cake/Program.cs
@@ -54,7 +54,7 @@ public sealed class PushTcOpenGroupPackages : FrostingTask<BuildContext>
             context.DotNetNuGetPush(nugetFile.FullName, new Cake.Common.Tools.DotNet.NuGet.Push.DotNetNuGetPushSettings()
             {
                 Source = "https://nuget.pkg.github.com/inxton/index.json",
-                ApiKey = System.Environment.GetEnvironmentVariable("TC_OPEN_GROUP_USER_PAT"),  
+                ApiKey = System.Environment.GetEnvironmentVariable("NUGET_TCOPEN"),  
                 SkipDuplicate = true
             });
         }
@@ -69,15 +69,15 @@ public sealed class ReleaseTask : FrostingTask<BuildContext>
     {
         
         {
-            var githubToken = context.Environment.GetEnvironmentVariable("TC_OPEN_GROUP_USER_PAT");
+            var githubToken = context.Environment.GetEnvironmentVariable("NUGET_TCOPEN");
             var githubClient = new GitHubClient(new ProductHeaderValue("TcOpen"));
             githubClient.Credentials = new Credentials(githubToken);
 
-            var doesExists = githubClient.Repository.Release.GetAll("TcOpenGroup", "TcOpen", ApiOptions.None).Result.Any(p => p.Name == GitVersionInformation.SemVer);
+            var doesExists = githubClient.Repository.Release.GetAll("Inxton", "TcOpen", ApiOptions.None).Result.Any(p => p.Name == GitVersionInformation.SemVer);
             if (!doesExists)
             {
                 var release = githubClient.Repository.Release.Create(
-                    "TcOpenGroup",
+                    "Inxton",
                     "TcOpen",
                     new NewRelease($"{GitVersionInformation.SemVer}")
                     {
